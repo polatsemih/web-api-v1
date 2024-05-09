0c0d0036b2e53e79854ebd7db6a377335178ac27
polatsemih <polatsemih@protonmail.com>
2024-05-09 17:25:09 +0300

    Entities Initialized

---
+++ Core/VkBank.Domain/Common/EntityBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VkBank.Domain.Common
+{
+    public class EntityBase : IEntityBase
+    {
+        public int Id { get; set; }
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime LastModifiedDate { get; set; } = DateTime.Now;
+    }
+}
